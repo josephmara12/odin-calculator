0b1ecb36f90c36c147944cce70b50d71bd08284f
josephmara12 <71110479+josephmara12@users.noreply.github.com>
2024-10-25 16:09:44 +1100

    Fixed firstfunc

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let num1 = ""
 let num2 = ""
 let operatorBeenChosen = 0 
 let operators = ['+', '-', '/', '*', '%', 'AC', '=']
-let numbers = ['1','2','3','4',5,'6','7','8','9','0']
 let currentOperator
+let calculationDone = false;
+let result;
+
 
 
 
@@ -17,43 +19,43 @@ let currentOperator
 
 //getValues is the main function that distinguishes between num1 and num2 
 
-
 function calculatorLogic() {
-    
+
+    if (calculationDone && !operators.includes(this.value)) {
+        screen.replaceChildren(); 
+        calculationDone = false; 
+    }
     getValues(this.value)
     displayValues(this.value)
     operate(this.value)
     clearValues(this.value)
-   
-
-
 }
-
 
 function getValues(buttonPressed) {
 
     const currentValue = buttonPressed
 
-    if (operators.includes(currentValue) && currentValue!= '=') {
-        currentOperator = currentValue
-        operatorBeenChosen += 1
-        screen.replaceChildren()
-        console.log(currentOperator)
-    }
+        if (operators.includes(currentValue) && currentValue != '=') {
+            currentOperator = currentValue
+            operatorBeenChosen = 1
+            screen.replaceChildren()
+            
+        }
 
-    if (operators.includes(currentValue) == false) {
-        if (operatorBeenChosen == 0) {
+        if (operators.includes(currentValue) == false) {
+            if (operatorBeenChosen == 0) {
                 num1 += currentValue
                 console.log(`num1 is now ${num1}`)
     
-        }
+            }
 
-        else if (operatorBeenChosen == 1) {
+            else if (operatorBeenChosen == 1) {
                 num2 += currentValue
                 console.log(`num2 is now ${num2}`)
             
+            }
         }
-    }
+
 }
 
 function displayValues(values) {
@@ -69,34 +71,41 @@ function displayValues(values) {
 function operate(buttonPressed) {
     if (buttonPressed == '=') {
         screen.replaceChildren()
-
-        let result;
         switch (currentOperator) {
         case '+':
                 result = Number(num1) + Number(num2);
-              
+                
+                
         break;
         case '-':
                 result = Number(num1) - Number(num2);
+                
             
         break;
         case '*':
                 result = Number(num1) * Number(num2);
                 
+                
         break;
         case '/':
                 result = Math.round((Number(num1) / Number(num2)) * 10000000) / 100000000
                 
-                
         break;
-}
-        displayValues(result)
+        }
         
+
+
+
+
+
+        displayValues(result)
+        num1 = ""
+        num2 = ""
+        operatorBeenChosen = 0
+        calculationDone = true;
     }
 
-
     }
-
 
 function clearValues(clearButton) {
     if (clearButton == 'AC') {
